eee778458982a9345d427d5fdd490755a85c21ab
jitterrs <133119191+jitterrs@users.noreply.github.com>
2025-11-15 15:55:46 +0300

    Delete src/AlgorithmFile.java

--- src/AlgorithmFile.java
+++
@@ -1,9 +0,0 @@
-// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
-public class AlgorithmFile {
-   public AlgorithmFile() {
-   }
-
-   public static void main(String[] var0) throws Exception {
-      System.out.println("HELL!");
-   }
-}
